947a8f99e155070c3725d832961fd5dbf2983009
brian@pandodev.com <brian@pandodev.com>
2023-03-31 14:05:01 -0400

    feat: extract

---
+++ extract.js
@@ -0,0 +1,24 @@
+// extract.js - Extracting data
+
+// Imports
+const fetch = require("./node-fetch");
+
+// Extract data
+const extract = async (url, log = false) => {
+
+    const fullUrl = `${process.env.NHL_BASE_URL}${url}`
+
+  // api call
+  const response = await fetch(fullUrl);
+  const data = await response.json();
+
+  // Tract Logging
+  if(log == true){
+    console.log(data);
+  }
+
+  // Return
+  return data;
+};
+
+module.exports = extract;
